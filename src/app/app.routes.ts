9a449150cab673d1696723861de1f7d39b8a0c01
Tim Schraepen <sch3lp@gmail.com>
2017-01-23 14:14:09 +0100

    be more specific about the app_routes type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import {ContactsListComponent} from "./contacts-list/contacts-list.component";
+import {Routes} from "@angular/router";
 
-export const APP_ROUTES = [
+export const APP_ROUTES:Routes = [
   { path: '', component: ContactsListComponent}
 ];
