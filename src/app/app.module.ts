d174383d4c584d9986e0595649a1021584252714
Tim Schraepen <sch3lp@gmail.com>
2017-03-11 18:05:54 +0100

    added ConfirmNavigationGuard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {MaterialModule} from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {HttpModule} from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule, OpaqueToken} from "@angular/core";
+import {MaterialModule} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
 import {environment} from "../environments/environment";
-import {FormsModule, NG_VALIDATORS, ReactiveFormsModule} from "@angular/forms";
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {APP_ROUTES} from "./app.routes";
-import {ContactsAppComponent} from './contacts.component';
+import {ContactsAppComponent} from "./contacts.component";
 import {ContactsService} from "./contacts.service";
-import {ContactsListComponent} from './contacts-list/contacts-list.component';
-import {ContactsEditorComponent} from './contacts-editor/contacts-editor.component';
-import {ContactsDetailComponent} from './contacts-detail/contacts-detail.component';
-import {ContactsDetailViewComponent} from './contacts-detail-view/contacts-detail-view.component';
+import {ContactsListComponent} from "./contacts-list/contacts-list.component";
+import {ContactsEditorComponent} from "./contacts-editor/contacts-editor.component";
+import {ContactsDetailComponent} from "./contacts-detail/contacts-detail.component";
+import {ContactsDetailViewComponent} from "./contacts-detail-view/contacts-detail-view.component";
 import {EventBusService} from "./event-bus.service";
-import { ContactsCreatorComponent } from './contacts-creator/contacts-creator.component';
-import { EmailValidator } from './email-validator.directive';
-import { EmailAvailabilityValidator } from './email-availability-validator.directive';
-import { ContactsDashboardComponent } from './contacts-dashboard/contacts-dashboard.component';
-import { AboutComponent } from './about/about.component';
+import {ContactsCreatorComponent} from "./contacts-creator/contacts-creator.component";
+import {EmailValidator} from "./email-validator.directive";
+import {EmailAvailabilityValidator} from "./email-availability-validator.directive";
+import {ContactsDashboardComponent} from "./contacts-dashboard/contacts-dashboard.component";
+import {AboutComponent} from "./about/about.component";
+
+export function doConfirm() {
+  return window.confirm('Navigate away without saving?');
+}
 
 @NgModule({
   declarations: [ContactsAppComponent, ContactsListComponent, ContactsDetailComponent, ContactsEditorComponent, ContactsDetailViewComponent, ContactsCreatorComponent, EmailValidator, EmailAvailabilityValidator, ContactsDashboardComponent, AboutComponent],
@@ -35,10 +38,11 @@ import { AboutComponent } from './about/about.component';
   providers: [
     ContactsService,
     EventBusService,
-    { provide: 'baseUrl', useValue: environment.baseUrl }
+    { provide: 'baseUrl', useValue: environment.baseUrl },
+    { provide: 'ConfirmNavigationGuard', useValue: doConfirm }
   ],
   bootstrap: [ContactsAppComponent]
 })
-export class ContactsModule {
+export class ContactsModule { }
 
-}
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const APP_ROUTES:Routes = [
     children: [
       {path: '', redirectTo: '/contact/0', pathMatch: 'full'},
       {path: 'contact/new', component: ContactsCreatorComponent},
-      {path: 'contact/:id/edit', component: ContactsEditorComponent},
+      {path: 'contact/:id/edit', component: ContactsEditorComponent, canDeactivate: ['ConfirmNavigationGuard']},
       {path: 'contact/:id', component: ContactsDetailViewComponent},
     ]
   },
